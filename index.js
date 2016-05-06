94aba0477e1dd360c353ab22af510116c7f024f7
Ali Roberts <ali.roberts3@gmail.com>
2016-05-06 13:03:50 +0100

    Patched index.js

--- index.js
+++ index.js
@@ -1,2 +1,64 @@
-var ReactNative = require('react-native')
-module.exports = ReactNative.NativeModules.Contacts
+/*
+* Patch for Siphon Sandbox
+* Orginal:
+* var ReactNative = require('react-native')
+* module.exports = ReactNative.NativeModules.Contacts
+*/
+
+var React = require('react-native');
+var { Platform } = React;
+
+module.exports = {
+  getAll: function() {
+    console.warn('Warning: getContacts returns dummy data in the Siphon Sandbox');
+    if (Platform.OS === 'ios') {
+      return [
+        {
+          recordID: 1,
+          familyName: 'Jung',
+          givenName: 'Carl',
+          middleName: '',
+          emailAddresses: [{
+            label: 'work',
+            email: 'carl-jung@example.com',
+          }],
+          phoneNumbers: [{
+            label: 'mobile',
+            number: '(555) 555-5555',
+          }],
+          thumbnailPath: '',
+        }
+      ];
+    } else {
+      return [
+        {
+          recordID: 1,
+          familyName: '',
+          givenName: 'Carl Jung',
+          middleName: '',
+          emailAddresses: [{
+            label: 'work',
+            email: 'carl-jung@example.com',
+          }],
+          phoneNumbers: [{
+            label: 'mobile',
+            number: '(555) 555-5555',
+          }],
+          thumbnailPath: '',
+        }
+      ];
+    }
+  },
+
+  addContact: function() {
+    console.warn('Warning: addContact is disabled in the Siphon Sandbox');
+  },
+
+  updateContact: function() {
+    console.warn('Warning: updateContact is disabled in the Siphon Sandbox');
+  },
+
+  deleteContact: function() {
+    console.warn('Warning: deleteContact is disabled in the Siphon Sandbox');
+  },
+};
